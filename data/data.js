e5f23d775ba40d39a26a4ad2ed36bcd6248f4462
LUKA <lukalavalferk@gmail.com>
2023-01-31 13:35:04 +0100

    Update data.js

--- data/data.js
+++ data/data.js
@@ -4,24 +4,24 @@ export default {
             type: "tree",
             category: "oak",
             location: {
-                latitude: 48.15372443592298, 
-                longitude: 11.585215203696562
+                latitude: 48.1572992, 
+                longitude: 11.5968628
             }
         },
         {
             type: "tree",
             category: "pine",
             location: {
-                latitude: 48.15357335792386, 
-                longitude: 11.586449422776434
+                latitude: 48.1572375, 
+                longitude: 11.5968092
             }
         },
         {
             type: "tree",
             category: "chestnut",
             location: {
-                latitude: 48.153730697247916,
-                longitude: 11.586419919181512
+                latitude: 48.1571964, 
+                longitude: 11.5968736
             }
         }
     ],
@@ -30,24 +30,24 @@ export default {
             type: "ground",
             category: "grass",
             location: {
-                latitude: 48.15349175889663,
-                longitude: 11.584006887570892
+                latitude: 48.157348947630034, 
+                longitude: 11.597298824580445
             }
         },
         {
             type: "ground",
             category: "water",
             location: {
-                latitude: 48.15443632948325, 
-                longitude: 11.586573844256163 
+                latitude: 48.15951814629954, 
+                longitude: 11.595583246767772
             }
         },
         {
             type: "ground",
             category: "gravel",
             location: {
-                latitude: 48.152810026896454,
-                longitude: 11.585973240551215
+                latitude: 48.1570077272161, 
+                longitude: 11.596272970449883
             }
         }
     ]
